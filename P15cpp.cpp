4f61c5c9db26eb38f5559374d214317814d0ce32
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2023-05-18 23:32:58 +0100

    letter pattern

---
+++ P15cpp.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+using namespace std;
+
+int ReadNumberPositive(string message) {
+    int Number;
+    cout << message << endl;
+    cin >> Number;
+    return Number;
+
+}
+
+
+int pattern(int Number) {
+
+
+    for (int i = 1; i <= Number; i++) {
+        for (int j = 1; j <= i; j++) {
+            char letter = static_cast<char>(i + 96);
+            cout << letter;
+        }
+        cout << endl;
+    }
+
+
+}
+
+
+int main() {
+    // Write C++ code here
+    int Number = ReadNumberPositive("entre un Number Positive");
+    pattern(Number);
+
+
+
+}
